29bb42e49be885d46bc3b4e46ba7797c3a8b65aa
radelweys <radelweys.30@gmail.com>
2026-05-07 00:43:20 +0800

    Add not expression for empty task validation

--- todolist.py
+++ todolist.py
@@ -1,29 +1,39 @@
 import tampilan as tp
+from data import data 
 tp.tampilanAwal()
 
-todoList = []
+listku = data()
+
+if not listku:
+  print('tugas kamu kosong')
+else:
+  print('Berikut tugas kamu:')
+
 
 def input_tugas():
   print('Masukann tugas kamu: ')
   i = 1
   while i <= 5:
     masukan_tugas = input('Tugas ke-{}: '.format(i))
-    tugas_baru = todoList.append(masukan_tugas)
+    listku.append(masukan_tugas)
     i+=1
-  return tugas_baru
+  return listku
 
   
   
 
-def tampilkan_tugas(tugas_baru=todoList):
+def tampilkan_tugas(tugas_baru=listku):
   for i in range(len(tugas_baru)):
     print('Tugas ke-{}: {}'.format(i+1, tugas_baru[i]))
 
 
 def main():
+  
   input_tugas()
   tp.tampilanHasil()
+
   tampilkan_tugas()
+  print('Kamu sudah berhasil mengisi tugas')
 
 
 main()
